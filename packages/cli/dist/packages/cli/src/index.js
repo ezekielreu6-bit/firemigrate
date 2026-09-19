8de073f3340f933b63d6a7e4aa7ef7c9ccfee0db
Ezekiel <ezekielreu6@gmail.com>
2026-09-19 19:58:06 +0000

    Move core sources to packages/core/src; stop tracking dist

--- packages/cli/dist/packages/cli/src/index.js
+++ packages/cli/dist/packages/cli/src/index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const domain_1 = require("../../../packages/core/src/domain");
+const domain_1 = require("../../core/src/domain");
 (0, domain_1.runCli)(process.argv.slice(2))
-    .then((output) => process.stdout.write(output))
+    .then((output) => {
+    process.stdout.write(output.endsWith('\n') ? output : `${output}\n`);
+})
     .catch((error) => {
-    process.stderr.write(`${error instanceof Error ? error.message : 'Unknown error'}\n`);
+    process.stderr.write(`${error instanceof Error ? error.message : String(error)}\n`);
     process.exitCode = 1;
+})
+    .finally(() => {
+    setTimeout(() => process.exit(process.exitCode ?? 0), 2000).unref();
 });
